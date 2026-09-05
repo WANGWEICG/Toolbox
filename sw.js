df15a2fbe34d35501dd4cd6af275667200f12a73
WANGWEICG <johnny0909@hotmail.com>
2026-09-05 23:57:04 +0800

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-const CACHE='wangwei-toolbox-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  const u=new URL(e.request.url);
-  if(u.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})));
-  }
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.registration.unregister());
 });
+
+self.addEventListener('fetch', () => {});
